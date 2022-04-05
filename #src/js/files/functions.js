530b6e581bfadac05319888198f553d52d848092
Danylo Kyrmach <tomahawk535@gmail.com>
2022-04-05 20:12:33 +0300

    add animation on scroll

--- #src/js/files/functions.js
+++ #src/js/files/functions.js
@@ -77,12 +77,12 @@ if (location.hash) {
 // 		}
 // 	});
 // };
-function menu_close() {
-	let iconMenu = document.querySelector(".icon-menu");
-	let menuBody = document.querySelector(".menu__body");
-	iconMenu.classList.remove("_active");
-	menuBody.classList.remove("_active");
-}
+// function menu_close() {
+// 	let iconMenu = document.querySelector(".icon-menu");
+// 	let menuBody = document.querySelector(".menu__body");
+// 	iconMenu.classList.remove("_active");
+// 	menuBody.classList.remove("_active");
+// }
 //=================
 //BodyLock
 function body_lock(delay) {
--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -7,3 +7,44 @@ if (iconMenu) {
         menuBody.classList.toggle('_active');
     });
 }
+
+//// ====== animation on scroll =========================================================================
+const  animations = document.querySelectorAll('.anim__item');
+
+if (animations.length >0 ) {
+    window.addEventListener('scroll', scrollAnimation);
+    function scrollAnimation(param) {
+        for(let index = 0; index < animations.length; index++ ) {
+            const animation = animations[index];
+            const animItemHeight = animation.offsetHeight;
+            const animItemOffset = offset(animation).top;
+
+            // === момент стратра анимации
+            const animStart = 4;
+
+            let animationPoint = window.innerHeight - animItemHeight / animStart;
+
+            if (animItemHeight >  window.innerHeight) {
+                animationPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+            if ((pageYOffset > animItemOffset - animationPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                animation.classList.add('active');
+            }else  {
+                if (!animation.classList.contains('anim__stop')) {
+                    animation.classList.remove('active');
+                }
+            }
+        }
+    }
+    // ======= Узнаем положение элемента относительно высоты экрана
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft}
+    }
+
+   setTimeout(() => {
+       scrollAnimation();
+   }, 300)
+}
